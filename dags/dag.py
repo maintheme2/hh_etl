1d7fca3a2aea3aa0cdb6398610b255a7a767f23c
maintheme <oligarh20031@mail.ru>
2024-07-11 18:26:34 +0300

    edit dag

--- dags/dag.py
+++ dags/dag.py
@@ -10,35 +10,41 @@ from airflow.operators.bash import BashOperator
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+from etls.hh_etl import extract, transform, load
+
 with DAG(
     'etl',
     description='A simple etl pipeline to extract and load data to postgresql database',
-    start_date=datetime(2023, 11, 23),
-    schedule_interval='@daily'
+    start_date=datetime(2024, 7, 10),
+    schedule_interval='@daily',
+    catchup=False
 ) as dag:
-    
+
     extract = PythonOperator(
         task_id='extract_data',
         python_callable=extract,
         op_kwargs={
-            'job_titles': ['postgreSQL', 'data engineer'],
+            'job_titles': ['etl-developer', 'data engineer'],
             'number_of_pages': 50
         }
     )
 
-    transform = BashOperator(
+    transform = PythonOperator(
         task_id='transform_data',
-        bash_command='jupyter nbconvert --execute --to notebook --inplace etl/transform.ipynb'
+        python_callable=transform,
+        op_kwargs={
+            'path': 'data'
+        }
     )
 
     load = PythonOperator(
         task_id='load_data',
         python_callable=load,
         op_kwargs={
-            'df': 'transformed.csv',
-            'db_name': 'postgres',
-            'db_user': 'temp_user',
-            'db_password': 'temp_password'
+            'df': './data/output/transformed.csv',
+            'db_name': 'vacancies',
+            'db_user': 'vacancies',
+            'db_password': 'vacancies'
         }
     )
 
